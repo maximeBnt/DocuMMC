fea3040470cb005c99de26c9c92403c980eaf91c
leducqc <charles.leducq@sts-sio-caen.info>
2014-11-18 16:55:58 +0100

    form_validation

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -25,6 +25,11 @@ class test extends CI_Controller{
         foreach ($this->collection->getItems() as $value)
             echo($value."<br>");
 
+        //$this->session->set_userdata('utilisateur', $user);
+
+        //$aUser= $this->session->userdata('utilisateur');
+        //echo($aUser->getNom());
+
     }
 
 
