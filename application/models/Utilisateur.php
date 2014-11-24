e1e07275ad3788bc0f1328a1440a9f53f00b6a6c
matthiaslecomte <matthiasou@yahoo.fr>
2014-11-24 10:29:47 +0100

    collection

---
+++ application/models/Utilisateur.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthiaslecomte
+ * Date: 24/11/14
+ * Time: 10:27
+ */
+
+class Utilisateur {
+    public function test() {
+        echo "salut";
+
+    }
+
+
+} 
